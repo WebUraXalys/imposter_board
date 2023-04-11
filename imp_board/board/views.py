82bda85f4603803854f54259e2f7cf35341a72e1
Igor4er <sherednihenko.ihor@gmail.com>
2023-04-11 23:06:05 +0300

    mark page users validation

--- imp_board/board/views.py
+++ imp_board/board/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from .forms import StudentValidation, StudVal
 from .models import *
 from django.core.mail import send_mail
+from django.core.exceptions import PermissionDenied
 from django.conf import settings
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 
 def choice_gr(request):
     return render(request, 'board/choice_group.html')
@@ -11,15 +12,19 @@ def choice_gr(request):
 def choice_fac(request):
     if request.method == "POST":
         form = StudentValidation(request.POST)
-        if form.is_valid:
-            grp = Group.objects.get(name=form.name)
-            grp.name = ""
+        if form.is_valid():
+            grp = form.cleaned_data['group']
             request.session['group_name'] = grp.name
 
-            user_mail = form.email
+            user_mail = form.cleaned_data['email']
+
+            kw = {
+                "groupname": grp.name
+            }
 
             send_mail("Оцінювання викладачів",
-            f" Ось ваше особисте посилання для оцінювання викладачів. Воно працює тільки з того девайсу з якого ви відправляти ваші дані. {reverse('main', groupname=grp.name)}",
+
+            f" Ось ваше особисте посилання для оцінювання викладачів. Воно працює тільки з того девайсу з якого ви відправляти ваші дані. {redirect('main', groupname=grp.name)}",
             settings.DEFAULT_FROM_MAIL, [user_mail])
             
             request.session['allow-group'] = grp.name
@@ -30,6 +35,14 @@ def choice_fac(request):
 
 
 def serve_main(request, groupname):
+    if not request.session.get("allow-group", None) == groupname:
+        # return PermissionDenied()
+        return HttpResponse("403 Forbidden", status=403)
+    grp = Group.objects.get(name=groupname)
+    context = {
+
+        "disciplines": grp.disciplines
+    }
     return render(request, 'board/main.html')
 
 def send_invitation(request):
