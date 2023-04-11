29e0c08a41c7578c790c9eb380fe27febfd5000a
Igor4er <sherednihenko.ihor@gmail.com>
2023-04-11 16:39:46 +0300

    added choice_fac view

--- imp_board/board/urls.py
+++ imp_board/board/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', choice_gr, name=""),
+    path('', choice_gr, name="choice"),
+    path('student/', choice_fac, name="choce_fac")
 ]
