c5805dd800cdabfe8fff3c6e62248d18b4f2457b
Igor4er <sherednihenko.ihor@gmail.com>
2023-04-11 16:23:32 +0300

    student validation form

--- imp_board/board/views.py
+++ imp_board/board/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, HttpResponse
+from .forms import student_validation
 
 # Create your views here.
 
 def choice_gr(request):
-    return render(request, 'board/choice_group.html')
-
+    return render(request, 'board/choice_group.html', context={
+        "form": student_validation
+    })
