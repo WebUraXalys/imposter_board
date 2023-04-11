b2e7e3e4e4f5b6fb12d0854369986932fcd2a3c4
Igor4er <sherednihenko.ihor@gmail.com>
2023-04-11 15:23:48 +0300

    ranmed template to choice

--- imp_board/board/views.py
+++ imp_board/board/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def choice_gr(request):
-    return render(request, 'board/index.html')
+    return render(request, 'board/choice_group.html')
