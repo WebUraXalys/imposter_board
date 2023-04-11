93af6582b3dd6959868f5e9c086fb434a7dff668
EtriKodoku <volodymyrpetriv2207@gmail.com>
2023-04-11 17:56:26 +0300

    Trash

--- imp_board/board/views.py
+++ imp_board/board/views.py
@@ -1,20 +1,15 @@
-from django.shortcuts import render, HttpResponse, redirect
+from django.shortcuts import render, HttpResponse
 from .forms import StudentValidation
-from .models import *
 from django.core.mail import send_mail
 
+# Create your views here.
 
 def choice_gr(request):
     return render(request, 'board/choice_group.html')
 
 def choice_fac(request):
-    if request.method == "POST":
-        form = StudentValidation(request.POST)
-        if form.is_valid:
-                grp = Group.objects.get(name=form.name)
-                request.session['group_name'] = grp.name
     return render(request, 'board/choice_fac.html', context={
-        "form": StudentValidation
+        "from": StudentValidation
     })
 
 
