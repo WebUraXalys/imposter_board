0bc4d9757d2e16c5fbd251b14517d17e546bfe06
Igor4er <sherednihenko.ihor@gmail.com>
2023-04-11 20:33:36 +0300

    fix some things in admin

--- imp_board/board/admin.py
+++ imp_board/board/admin.py
@@ -13,9 +13,7 @@ class TeacherAdmin(admin.ModelAdmin):
 
 class DisciplineAdmin(admin.ModelAdmin):
     list_display = ['name', 'teacher']
-class UserProfileInline(admin.StackedInline):
-    model = Group
-    filter_horizontal = ('disciplines',)
+
 class GroupAdmin(admin.ModelAdmin):
     list_display = ['name', 'disciplines', 'faculty']
 
@@ -37,11 +35,6 @@ class AverageMarkAdmin(admin.ModelAdmin):
 admin.site.register(Faculty, FacultyAdmin)
 admin.site.register(Teacher, TeacherAdmin)
 admin.site.register(Discipline, DisciplineAdmin)
-<<<<<<< HEAD
-admin.site.register(Mark, MarkAdmin)
-admin.site.register(Group, GroupAmin)
-=======
 admin.site.register(Group, GroupAdmin)
 admin.site.register(Mark, MarkAdmin)
 admin.site.register(AverageMark, AverageMarkAdmin)
->>>>>>> 7fa9e2bce3fee318ffd5b90d74f0f8a628938dfe
