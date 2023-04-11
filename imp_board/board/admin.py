5d2101acd08f80957b691363c5d833f4de258b7a
EtriKodoku <volodymyrpetriv2207@gmail.com>
2023-04-11 16:26:15 +0300

    Created models

--- imp_board/board/admin.py
+++ imp_board/board/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Discipline, Mark
+
 
 # Register your models here.
+class DisciplineAdmin(admin.ModelAdmin):
+    list_display = ['name', 'teacher']
+
+
+class MarkAdmin(admin.ModelAdmin):
+    list_display = ['group', 'quality', 'methodological_support', 'objectivity', 'discipline']
+
+
+admin.site.register(Discipline, DisciplineAdmin)
+admin.site.register(Mark, MarkAdmin)
--- imp_board/board/models.py
+++ imp_board/board/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
 
 # Create your models here.
+class Teacher(models.Model):
+    name = models.CharField(max_length=20, verbose_name="Teacher name")
+
+
+class Discipline(models.Model):
+    name = models.CharField(max_length=20, verbose_name="Discipline name")
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+
+
+class Mark(models.Model):
+    group = models.CharField(max_length=6, verbose_name="Group name")
+    quality = models.IntegerField(default=5, validators=[MaxValueValidator(10),
+                                                        MinValueValidator(1)])
+    methodological_support = models.IntegerField(default=5, validators=[MaxValueValidator(10),
+                                                                       MinValueValidator(1)])
+    objectivity = models.IntegerField(default=5, validators=[MaxValueValidator(10),
+                                                            MinValueValidator(1)])
+    discipline = models.ForeignKey(Discipline, on_delete=models.CASCADE)
+    semester = models.IntegerField(verbose_name="Semester number", validators=[MaxValueValidator(10),
+                                                                              MinValueValidator(1)])
+
+
+class AverageMark(models.Model):
+    group = models.CharField(max_length=6, verbose_name="Group name")
+    quality = models.IntegerField(default=5, validators=[MaxValueValidator(10),
+                                                        MinValueValidator(1)])
+    methodological_support = models.IntegerField(default=5, validators=[MaxValueValidator(10),
+                                                                       MinValueValidator(1)])
+    objectivity = models.IntegerField(default=5, validators=[MaxValueValidator(10),
+                                                            MinValueValidator(1)])
+    discipline = models.ForeignKey(Discipline, on_delete=models.CASCADE)
+    semester = models.IntegerField(verbose_name="Semester number", validators=[MaxValueValidator(10),
+                                                                              MinValueValidator(1)])
