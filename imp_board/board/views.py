43bbb0f7be48bd6f1bc1436dbb0311e637203a0f
Igor4er <sherednihenko.ihor@gmail.com>
2023-04-11 17:27:07 +0300

    logic

--- imp_board/board/views.py
+++ imp_board/board/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, HttpResponse
-from .forms import student_validation
+from django.shortcuts import render, HttpResponse, redirect
+from .forms import StudentValidation
 from .models import *
 
 # Create your views here.
@@ -8,6 +8,11 @@ def choice_gr(request):
     return render(request, 'board/choice_group.html')
 
 def choice_fac(request):
+    if request.method == "POST":
+        form = StudentValidation(request.POST)
+        if form.is_valid:
+                grp = Group.objects.get(name=form.name)
+                request.session['group_name'] = grp.name
     return render(request, 'board/choice_fac.html', context={
-        "form": student_validation
+        "form": StudentValidation
     })
