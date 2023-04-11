437fe690de9ac545d93a7717c5d56599b09458a9
Igor4er <sherednihenko.ihor@gmail.com>
2023-04-11 16:24:39 +0300

    comm form incl

--- imp_board/board/views.py
+++ imp_board/board/views.py
@@ -4,6 +4,9 @@ from .forms import student_validation
 # Create your views here.
 
 def choice_gr(request):
-    return render(request, 'board/choice_group.html', context={
-        "form": student_validation
+    return render(request, 'board/choice_group.html')
+
+def choice_fac(request):
+    return render(request, 'board/choice_fac.html', context={
+        "from": student_validation
     })
